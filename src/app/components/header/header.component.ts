2760b9eaa650cea121e08d9f1baff7be90a5110b
brandonhach <specialcambo@gmail.com>
2025-01-20 14:09:07 -0500

    created basic header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
 	selector: 'app-header',
 	standalone: true,
-	imports: [MatToolbarModule, MatIconModule],
+	imports: [MatToolbarModule, MatIconModule, MatBadgeModule, MatMenuModule],
 	templateUrl: './header.component.html',
 	styles: ``,
 })
