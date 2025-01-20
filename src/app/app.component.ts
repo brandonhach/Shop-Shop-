e9f936feea47989997a49074dedd836c52d6f349
brandonhach <specialcambo@gmail.com>
2025-01-20 11:44:00 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet, HeaderComponent],
+	template: ` <app-header> <router-outlet /> </app-header> `,
+	styles: [],
 })
 export class AppComponent {
-  title = 'store';
+	title = 'Shop Shop!';
 }
